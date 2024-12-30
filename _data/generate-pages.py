91775bdca7d979c70e23a61e1e2d39ad9c37e8d4
John Scott <john@jjrscott.com>
2024-12-30 14:09:23 +0000

    Update favicon

--- _data/generate-pages.py
+++ _data/generate-pages.py
@@ -244,7 +244,7 @@ index_page = """<!DOCTYPE html>
     <link rel="stylesheet" href="assets/index.css">
 
     <link rel="icon" type="image/png" href="/favicon.png">
-        
+    <link rel="icon" type="image/png" href="/favicon-light.png" media="(prefers-color-scheme: light)">
     <link rel="icon" type="image/png" href="/favicon-dark.png" media="(prefers-color-scheme: dark)">
 
 
